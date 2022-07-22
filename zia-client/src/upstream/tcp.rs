19b0bb9806c1ca3d8c874ede9febe0ba5a7185c1
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-07-22 20:06:50 +0200

    Improved websocket stack

--- zia-client/src/upstream/tcp.rs
+++ zia-client/src/upstream/tcp.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use tokio::net::UdpSocket;
 use url::Url;
 
-use zia_common::{datagram_buffer, process_udp_over_tcp, Stream};
+use zia_common::{process_udp_over_tcp, Stream};
 
 pub(crate) async fn transmit(
   socket: UdpSocket,
@@ -12,12 +12,14 @@ pub(crate) async fn transmit(
 ) -> anyhow::Result<()> {
   let upstream = Stream::connect(upstream, proxy).await?;
 
-  let mut tmp_buffer = datagram_buffer();
-  let (_udp_read_len, udp_peer_addr) = socket.peek_from(tmp_buffer.as_mut()).await?;
+  {
+    let mut tmp_buffer = [0; u16::MAX as usize];
+    let (_udp_read_len, udp_peer_addr) = socket.peek_from(tmp_buffer.as_mut()).await?;
 
-  // Connect the UDP socket to whoever sent the first datagram. This is where
-  // all the returned traffic will be sent to.
-  socket.connect(udp_peer_addr).await?;
+    // Connect the UDP socket to whoever sent the first datagram. This is where
+    // all the returned traffic will be sent to.
+    socket.connect(udp_peer_addr).await?;
+  };
 
   process_udp_over_tcp(socket, upstream, Some(Duration::from_secs(60))).await;
 
--- zia-client/src/upstream/ws.rs
+++ zia-client/src/upstream/ws.rs
@@ -1,80 +1,29 @@
-use std::sync::Arc;
+use std::time::Duration;
 
-use futures_util::{SinkExt, StreamExt};
 use tokio::net::UdpSocket;
-use tokio::sync::RwLock;
-use tokio::task::JoinHandle;
-use tokio::try_join;
 use tokio_tungstenite::client_async;
-use tokio_tungstenite::tungstenite::Message;
-use tracing::warn;
 use url::Url;
-use zia_common::Stream;
+
+use zia_common::{process_udp_over_ws, Stream};
 
 pub(crate) async fn transmit(
   socket: UdpSocket,
   upstream: &Url,
   proxy: &Option<Url>,
 ) -> anyhow::Result<()> {
-  let stream = Stream::connect(upstream, proxy).await?;
+  let raw_upstream = Stream::connect(upstream, proxy).await?;
+  let (upstream, _) = client_async(upstream, raw_upstream).await?;
 
-  // websocket handshake
-  let (upstream, _) = client_async(upstream, stream).await?;
-  let (mut wu, mut ru) = upstream.split();
+  {
+    let mut tmp_buffer = [0; u16::MAX as usize];
+    let (_udp_read_len, udp_peer_addr) = socket.peek_from(tmp_buffer.as_mut()).await?;
 
-  // udp remote address -> no active connection
-  let remote_addr = RwLock::new(None);
-
-  let socket_a = Arc::new(socket);
-  let socket_b = socket_a.clone();
-
-  let remote_addr_a = Arc::new(remote_addr);
-  let remote_addr_b = remote_addr_a.clone();
-
-  let client_to_server = async move {
-    // TODO: can this be abused -> receive endless data from the other udp side
-    let mut buf = vec![0; 65507];
-    let (mut read, mut new_addr) = socket_a.recv_from(&mut buf).await?;
-    *remote_addr_a.write().await = Some(new_addr);
-    while read != 0 {
-      wu.send(Message::binary(&buf[..read])).await?;
-      (read, new_addr) = socket_a.recv_from(&mut buf).await?;
-      if remote_addr_a
-        .read()
-        .await
-        .map(|addr| addr != new_addr)
-        .unwrap_or(false)
-      {
-        *remote_addr_a.write().await = Some(new_addr);
-      }
-    }
-    wu.close().await?;
-    Ok::<_, anyhow::Error>(())
+    // Connect the UDP socket to whoever sent the first datagram. This is where
+    // all the returned traffic will be sent to.
+    socket.connect(udp_peer_addr).await?;
   };
 
-  let server_to_client = async move {
-    while let Some(next) = ru.next().await {
-      let msg = next?;
-      if let Some(remote_addr) = *remote_addr_b.read().await {
-        socket_b.send_to(&msg.into_data(), remote_addr).await?;
-      } else {
-        warn!(
-          "Dropped a websocket message of length {} because no downstream client is registered.",
-          msg.len()
-        );
-      }
-    }
-    Ok::<_, anyhow::Error>(())
-  };
-
-  try_join!(
-    flat(tokio::spawn(client_to_server)),
-    flat(tokio::spawn(server_to_client)),
-  )?;
+  process_udp_over_ws(socket, upstream, Some(Duration::from_secs(60))).await;
 
   Ok(())
-}
-
-async fn flat<V>(r: JoinHandle<anyhow::Result<V>>) -> anyhow::Result<V> {
-  r.await?
 }
